37ed42feda8f39479682964c7be1da45d6be83ef
Josh Lohner <lohnerj1@mymail.nku.edu>
2023-12-14 17:01:23 -0500

    Update index

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Mission Statement</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Mission Statement!</a></li>
                         
                     </ul>
                     <a class="btn btn-outline-light" href="./5/login.php">Login</a>
